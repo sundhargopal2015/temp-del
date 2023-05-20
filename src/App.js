7ec73bdaa9f601d88f0a313e5d5af4694fdb46f8
sundaramoorthy <sundaramoorthy.gopalakrishnan@whatfix.com>
2023-05-20 10:16:08 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn  with some examples React
         </a>
       </header>
     </div>
